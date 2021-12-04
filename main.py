39dd4d2cdaabbdc4b38fce5f25666aa44a578d86
whitenight1357 <notwhitenight1357@gmail.com>
2021-12-04 12:55:51 +0900

    add wall collision(not perfect)

--- main.py
+++ main.py
@@ -27,35 +27,32 @@ screen.fill([227, 255, 250])
 printtext("Check Python Prompt First")
 pygame.display.flip()
 
-selected_frame = int(input("process until frame#: "))
-xpos = float(input("start x position: "))
-ypos = float(input("start y position: "))
-xspeed = float(input("start xspeed value: "))
-yspeed = float(input("start yspeed value: "))
-xspeedchange = float(input("xspeed change per frame value: "))
-yspeedchange = float(input("yspeed change per frame value: "))
+size = 20
+selected_frame = 90  # int(input("process until frame#: "))
+xpos = 100 - size  # float(input("start x position: "))
+ypos = 200 - size  # float(input("start y position: "))
+xspeed = 3  # float(input("start xspeed value: "))
+yspeed = 0  # float(input("start yspeed value: "))
+xspeedchange = 0  # float(input("xspeed change per frame value: "))
+yspeedchange = 0  # float(input("yspeed change per frame value: "))
+wallxstart = 300
+wallystart = 100
+wallxwidth = 50
+wallywidth = 200
 
 while not end:
     clock.tick(30)
 
-#    if end == True:
-#        break
-
+    # if end == True:
+    #     break
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             end = True
 
+    # draw background
     screen.fill([227, 255, 250])
 
-    printtext("frame: " + str(frame), 'BLACK', (50, 30))
-    printtext("xpos: " + str(xpos), 'BLACK', (50, 50))
-    printtext("ypos: " + str(ypos), 'BLACK', (50, 70))
-    printtext("xspeed: " + str(xspeed), 'BLACK', (50, 90))
-    printtext("yspeed: " + str(yspeed), 'BLACK', (50, 110))
-
-#    rect(on what, color, [xposition, yposition, xsize, ysize], line width:null to fill)
-    pygame.draw.rect(screen, [0, 0, 0], [xpos, ypos, 20, 20], 1)
-
+    # change value
     xpos = xpos + xspeed
     ypos = ypos + yspeed
     xspeed = xspeed + xspeedchange
@@ -65,6 +62,35 @@ while not end:
         end = True
     else:
         frame = frame + 1
+
+    # collision check
+    if wallxstart - size < xpos < wallxstart + wallxwidth + size \
+       and\
+       wallystart - size < ypos < wallystart + wallywidth + size:
+        if xspeed >= 0:
+            xpos = wallxstart - size
+            xspeed = 0
+        if xspeed < 0:
+            xpos = wallxstart + wallxwidth + size
+            xspeed = 0
+        if yspeed >= 0:
+            ypos = wallystart - size
+            yspeed = 0
+        if yspeed < 0:
+            ypos = wallystart + wallywidth + size
+            yspeed = 0
+
+    # print debug info
+    printtext("frame: " + str(frame), 'BLACK', (50, 30))
+    printtext("xpos: " + str(xpos), 'BLACK', (50, 50))
+    printtext("ypos: " + str(ypos), 'BLACK', (50, 70))
+    printtext("xspeed: " + str(xspeed), 'BLACK', (50, 90))
+    printtext("yspeed: " + str(yspeed), 'BLACK', (50, 110))
+
+    # draw wall and player
+    # rect(on what, color, [xposition, yposition, xsize, ysize], line width:null to fill)
+    pygame.draw.rect(screen, [130, 130, 130], [wallxstart, wallystart, wallxwidth, wallywidth])
+    pygame.draw.rect(screen, [0, 0, 0], [xpos, ypos, 20, 20], 1)
 
     pygame.display.flip()
 
