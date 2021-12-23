85197c58ea2a2cf99acc80495b68b7de01e56c73
whitenight1357 <notwhitenight1357@gmail.com>
2021-12-23 13:54:45 +0900

    initialized y physics (not perfect yet)

--- main.py
+++ main.py
@@ -14,6 +14,24 @@ pygame.display.set_caption("pysmb1")
 clock = pygame.time.Clock()
 end = False
 
+player = pygame.image.load("resource/player.png")
+
+size = 16 * pxsize
+xpos = 0
+ypos = 0
+xspeed = 0
+yspeed = 0
+realxspeed = "0x0000"
+realyspeed = "0x0000"
+beforejumpxspeed = "0x0000"
+ledgerunoffspeed = "0x0000"
+wallxstart = 0
+wallystart = 400
+wallxwidth = 512
+wallywidth = 100
+on_ground = False
+debugging = False
+
 
 def printtext(msg, color=BLACK, pos=(50, 50)):
     textsurface = font.render(msg, True, pygame.Color(color), None)
@@ -23,18 +41,57 @@ def printtext(msg, color=BLACK, pos=(50, 50)):
     screen.blit(textsurface, textrect)
 
 
-player = pygame.image.load("resource/player.png")
+def jump():
+    global realxspeed
+    global realyspeed
+    global beforejumpxspeed
+    if abs(int(realxspeed, 16)) < int("0x1000", 16):
+        realyspeed = "-0x4000"
+        beforejumpxspeed = realxspeed
+    elif int("0x1000", 16) <= abs(int(realxspeed, 16)) <= int("0x24ff", 16):
+        realyspeed = "-0x4000"
+        beforejumpxspeed = realxspeed
+    elif int("0x2500") <= abs(int(realxspeed, 16)):
+        realyspeed = "-0x5000"
+        beforejumpxspeed = realxspeed
 
-size = 8 * pxsize
-xpos = 0
-ypos = 0
-xspeed = 0
-yspeed = 0
-wallxstart = 150
-wallystart = 150
-wallxwidth = 100
-wallywidth = 100
-debugging = False
+
+def y_psysics(holding_a):
+    global realyspeed
+    global beforejumpxspeed
+    if int(realyspeed, 16) > 0 and holding_a:
+        if abs(int(beforejumpxspeed, 16)) < int("0x1000", 16):
+            realyspeed = hex(int(realyspeed, 16) + int("0x0200", 16))
+        if int("0x1000", 16) <= abs(int(beforejumpxspeed, 16)) <= int("0x24ff", 16):
+            realyspeed = hex(int(realyspeed, 16) + int("0x01e0", 16))
+        if int("0x2500", 16) <= abs(int(beforejumpxspeed, 16)):
+            realyspeed = hex(int(realyspeed, 16) + int("0x0280", 16))
+    else:
+        if abs(int(beforejumpxspeed, 16)) < int("0x1000", 16):
+            realyspeed = hex(int(realyspeed, 16) + int("0x0700", 16))
+        if int("0x1000", 16) <= abs(int(beforejumpxspeed, 16)) <= int("0x24ff", 16):
+            realyspeed = hex(int(realyspeed, 16) + int("0x0600", 16))
+        if int("0x2500", 16) <= abs(int(beforejumpxspeed, 16)):
+            realyspeed = hex(int(realyspeed, 16) + int("0x0900", 16))
+
+
+def fillnull(hexvalue):
+    if len(hexvalue) == 3 and int(hexvalue, 16) > 0:
+        hexvalue = hexvalue[:2] + "000" + hexvalue[2:]
+    elif len(hexvalue) == 4 and int(hexvalue, 16) > 0:
+        hexvalue = hexvalue[:2] + "00" + hexvalue[2:]
+    elif len(hexvalue) == 5 and int(hexvalue, 16) > 0:
+        hexvalue = hexvalue[:2] + "0" + hexvalue[2:]
+    elif len(hexvalue) == 4 and int(hexvalue, 16) < 0:
+        hexvalue = hexvalue[:3] + "000" + hexvalue[3:]
+    elif len(hexvalue) == 5 and int(hexvalue, 16) < 0:
+        hexvalue = hexvalue[:3] + "00" + hexvalue[3:]
+    elif len(hexvalue) == 6 and int(hexvalue, 16) < 0:
+        hexvalue = hexvalue[:3] + "0" + hexvalue[3:]
+    elif int(hexvalue, 16) == 0:
+        hexvalue = "0x0000"
+    return hexvalue
+
 
 while not end:
     # set framerate
@@ -53,29 +110,43 @@ while not end:
     # get keyboard input
     keys = pygame.key.get_pressed()
 
-    if not keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
-        yspeed = 0
-    elif keys[pygame.K_UP]:
-        yspeed = -3
-    elif keys[pygame.K_DOWN]:
-        yspeed = 3
-    else:
-        yspeed = 0
-    if not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+    if not keys[pygame.K_a] and not keys[pygame.K_d]:
         xspeed = 0
-    elif keys[pygame.K_LEFT]:
+    elif keys[pygame.K_a] and keys[pygame.K_d]:
+        xspeed = 0
+    elif keys[pygame.K_a]:
         xspeed = -3
-    elif keys[pygame.K_RIGHT]:
+    elif keys[pygame.K_d]:
         xspeed = 3
-    else:
+
+    if keys[pygame.K_k] and on_ground:
+        jump()
+    if keys[pygame.K_k] and not on_ground:
+        y_psysics(True)
+    elif not keys[pygame.K_k] and not on_ground:
+        y_psysics(False)
+
+    if debugging and keys[pygame.K_r]:
+        xpos = 0
+        ypos = 0
         xspeed = 0
+        yspeed = 0
+        realxspeed = "0x0000"
+        realyspeed = "0x0000"
+        beforejumpxspeed = "0x0000"
+        ledgerunoffspeed = "0x0000"
+        on_ground = False
 
     # draw background
     screen.fill([227, 255, 250])
 
     # change value
-    xpos = xpos + xspeed
-    ypos = ypos + yspeed
+    realxspeed = fillnull(realxspeed)
+    realyspeed = fillnull(realyspeed)
+    xspeed = int(realxspeed[0:-3], 16)
+    yspeed = int(realyspeed[0:-3], 16)
+    xpos = (xpos / pxsize + xspeed) * pxsize
+    ypos = (ypos / pxsize + yspeed) * pxsize
 
     # collision check
     if wallxstart - size < xpos < wallxstart + wallxwidth \
@@ -90,6 +161,7 @@ while not end:
         if yspeed > 0:
             ypos = wallystart - size
             yspeed = 0
+            on_ground = True
         if yspeed < 0:
             ypos = wallystart + wallywidth
             yspeed = 0
