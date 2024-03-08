90b7c5e9f7369f82dd87fd26c123156c524c3a9c
WhiteNight1357 <notwhitenight1357@gmail.com>
2024-03-08 10:20:06 +0900

    fixed tile display

--- tile.py
+++ tile.py
@@ -18,13 +18,13 @@ def tile_draw(level, campos, surface, pxsize):
         for _ in row:
             tile = level[listypos][listxpos]
             if tile is None or xpos - campos < -16 * pxsize or xpos - campos > 256 * pxsize:
-                xpos += 16 * pxsize
+                xpos += 16
                 listxpos += 1
                 continue
-            surface.blit(resources.tileset[tile], (xpos - campos, ypos))
-            xpos += 16 * pxsize
+            surface.blit(resources.tileset[tile], ((xpos - campos) * pxsize, ypos * pxsize))
+            xpos += 16
             listxpos += 1
-        ypos += 16 * pxsize
+        ypos += 16
         xpos = 0
         listypos += 1
         listxpos = 0
